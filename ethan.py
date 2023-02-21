fd912db3eafd4eab8559a00c71bda4eed020414c
Ethan <ethan.mache@gmail.com>
2023-02-21 15:52:46 +0100

    fusion d'arbre

--- ethan.py
+++ ethan.py
@@ -7,13 +7,24 @@ def comptage_lettre(text):
         liste.append((elem, text.count(elem)))
     return sorted(liste, key=lambda x : x[1])
 
-class ArbreB(object):
-    def __init__(self, racine, nb_sommet):
-        self.racine = racine
-        self.nb_sommet = nb_sommet
+class Sommet(object):
 
-    def insertion_sommet(self):
-        pass
+    def __init__(self, valeur, nom, fils_droit=None, fils_gauche=None):
+        self.valeur = valeur
+        self.nom = nom
+        self.fils_droit = fils_droit
+        self.fils_gauche = fils_gauche
+
+    def affichage(self):
+        return self.valeur, self.nom, self.fils_droit, self.fils_gauche
+
+    def rename_sommet(self, new_name):
+        self.nom = new_name
+
+
+class ArbreB(object):
+    def __init__(self, racine=None):
+        self.racine = racine
 
     def suppression_sommet(self):
         pass
@@ -21,29 +32,23 @@ class ArbreB(object):
     def recherche_sommet(self):
         pass
 
-    def fusion_arbre(self):
-        pass
+    def fusion_arbre(self, sommet1, sommet2):
+        return Sommet(sommet1.valeur + sommet2.valeur, sommet1.nom + sommet2.nom, sommet1, sommet2)
 
-class Sommet(ArbreB):
 
-    def __init__(self, valeur, nom, racine="self", nb_sommet=1):
-        super().__init__(racine, nb_sommet)
-        self.valeur = valeur
-        self.nom = nom
-
-    def affichage(self):
-        print(self.valeur, self.nom)
-
-    def rename_sommet(self, new_name):
-        self.nom = new_name
 
 test = comptage_lettre("bonjour comment ca va ?")
 print(test)
 l = []
-for elem in test:
-    l.append(Sommet(elem[1], elem[0]))
-for i in range(len(l)):
-    l[i].affichage()
+for nom, val in test:
+    l.append(Sommet(val, nom))
+#for i in range(len(l)):
+#    l[i].affichage()
 
 #l[-1].rename_sommet("test")
 #l[-1].affichage()
+
+l[0] = ArbreB.fusion_arbre(ArbreB, l[0], l[1])
+del l[1]
+print(l[0].affichage())
+print(l[1].affichage())
