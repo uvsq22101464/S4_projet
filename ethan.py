df1cb334db1b7ffa91480c6da98927b2204d91df
uvsq22101464 <91185466+uvsq22101464@users.noreply.github.com>
2023-04-16 19:49:06 +0200

    Update ethan.py

--- ethan.py
+++ ethan.py
@@ -1,3 +1,4 @@
+import math
 def comptage_lettre(text):
     """prend une chaîne de caractère en entrée et renvoi
     une liste de tuplecontenant les lettres qui apparaissent
@@ -11,24 +12,42 @@ class ArbreB(object):
     def __init__(self, racine=None):
         self.racine = racine
 
+    def getRacine(self):
+        return self.racine
+    def setRacine(self, nouvRacine):
+        self.racine = nouvRacine
+
     def recherche_sommet(self, nom):
         """Prend un ArbreB en entré ainsi qu'un nom de Sommet à rechercher dans l'arbre
         et renvois le Sommet correspondant ou None sinon"""
         if type(self) == ArbreB:
-            return self.racine.recherche_sommet(nom)
+            return self.getRacine().recherche_sommet(nom)
 
     def fusion_arbre(self, sommet1, sommet2):
         """Prend deux Sommet en entré et renvois un nouveau Sommet qui est le père des deux Sommets
         avec les valeurs des deux combinner"""
         return Sommet(sommet1.valeur + sommet2.valeur, sommet1.nom + sommet2.nom, sommet1, sommet2)
     
-    def parcour_arbre(self):  # Inutile pour le moment et surrement plus tard
-        def parcour_profondeur(self):
-            if type(self) == ArbreB:
-                self = self.racine
-            if self.fils_gauche != None and self.fils_droit != None:
-                return parcour_profondeur(self.fils_gauche), parcour_profondeur(self.fils_droit)
-        parcour_profondeur(self)
+    def profondeur_arbre(self, profondeur=0):
+        if type(self) == ArbreB:
+            self = self.getRacine()
+        if self.getGauche() != None and self.getDroit() != None:
+            profondeur = max(self.getGauche().profondeur_arbre(profondeur+1), self.getDroit().profondeur_arbre(profondeur+1))
+        return profondeur
+    
+    """def largeur_coter(self):
+        if type(self) == ArbreB:
+            self = self.getRacine()
+        dico = self.codage()
+        list_key = list(dico.values())
+        return len(list_key[0]) + len(list_key[-1])"""
+    
+    def largeur_arbre(self):
+        if type(self) == ArbreB:
+            self = self.getRacine()
+        dico = self.codage()
+        list_key = list(dico.values())
+        return len(list_key[0]) + len(list_key[-1]), len(list_key[0]), len(list_key[-1])
 
     def create_tree(liste_sommet):
         """Prend une liste de Sommet en entré et retourne un objet ArbreB dont la racine est le sommet de plus haute valeur"""
@@ -49,6 +68,28 @@ class ArbreB(object):
     def suppression_sommet(self, nom, liste_sommet):
         liste_sommet.remove(self.recherche_sommet(nom))
         return ArbreB.create_tree(liste_sommet)
+    
+    def dessinerArbre(self, canvas, largeur_canvas, x=0, y=0, cpt=1, largeur=0, profondeur=0, taille=[0, 0], posXSuivant=0):
+        if type(self) == ArbreB:
+            self = self.getRacine()
+            posXSuivant = largeur[0] ** math.log2(profondeur) * profondeur # le * repésente l'écart qu'on veut donner/ encore a déterminer
+            try:  # quand il y a un seul carac on a math domain error / peut être aussi pour d'autre taille de texte j'ai pas tout regarder
+                taille = (largeur[1] * posXSuivant / 2**(math.log2(largeur[1])-1), largeur[2] * posXSuivant / 2**(math.log2(largeur[2])-1))
+            except:
+                pass
+        posYSuivant = largeur_canvas // profondeur * cpt
+        if self.getGauche() == None and self.getDroit() == None:
+            color = "royalblue"
+        else:
+            color = "red"
+            canvas.create_line(x, y, x-posXSuivant, posYSuivant, arrow="last", fill="black")
+            canvas.create_line(x, y, x+posXSuivant, posYSuivant, arrow="last", fill="black")
+        canvas.create_oval(x-10, y, x+10, y+20, fill=color)
+        canvas.create_text(x, y+15, text=self.getNom(), fill="black")
+        if self.getGauche() != None and self.getDroit() != None:
+            self.getGauche().dessinerArbre(canvas, largeur_canvas, x-posXSuivant, posYSuivant, cpt+1, largeur, profondeur, taille, posXSuivant/2)
+            self.getDroit().dessinerArbre(canvas, largeur_canvas, x+posXSuivant, posYSuivant, cpt+1, largeur, profondeur, taille, posXSuivant/2)
+
     
 
 class Sommet(ArbreB):
@@ -76,7 +117,7 @@ class Sommet(ArbreB):
         self.fils_droit = newDroit
 
     def __str__(self):
-        return f"{self.valeur, self.nom, self.fils_gauche, self.fils_droit}"
+        return f"{self.getValeur(), self.getNom(), self.getGauche(), self.getDroit()}"
     
         
     def make_list(liste_occurence : list):
@@ -93,31 +134,46 @@ class Sommet(ArbreB):
             liste_sommet.append(Sommet(valeur, nom))
         return liste_sommet
 
-    def codage(self, code_sommet="", dico={}):
-        if self.fils_gauche != None:
-            self.fils_gauche.codage(code_sommet+"0", dico)
-        if self.fils_droit != None:
-            self.fils_droit.codage(code_sommet+"1", dico)
-        if len(self.nom) == 1:
-            dico[self.nom] = code_sommet
+    def codage(self, code_sommet="", dico={}, largeur=False):
+        if self.getGauche() != None:
+            self.getGauche().codage(code_sommet+"0", dico, largeur)
+        if self.getDroit() != None:
+            self.getDroit().codage(code_sommet+"1", dico, largeur)
+        if len(self.getNom()) == 1 and largeur == False:
+            dico[self.getNom()] = code_sommet
+        if largeur != False:
+            dico[self.getNom()] = code_sommet 
         return dico
     
     def recherche_sommet(self, nom):
-            if self.nom == nom:
+            if self.getNom() == nom:
                 return self
-            elif self.fils_gauche != None and self.fils_droit != None:
-                return self.fils_gauche.recherche_sommet(nom) or self.fils_droit.recherche_sommet(nom)
+            elif self.getGauche() != None and self.getDroit() != None:
+                return self.getGauche().recherche_sommet(nom) or self.getDroit().recherche_sommet(nom)
 
+    def dessinerArbre(self, canvas, largeur_canvas, x=0, y=0, cpt=1, largeur=0, profondeur=0, taille=[0, 0], posXSuivant=0):
+        posYSuivant = largeur_canvas // profondeur * cpt
+        if self.getGauche() == None and self.getDroit() == None:
+            color = "royalblue"
+        else:
+            color = "red"
+            canvas.create_line(x, y, x-posXSuivant, posYSuivant, arrow="last", fill="black")
+            canvas.create_line(x, y, x+posXSuivant, posYSuivant, arrow="last", fill="black")
+        canvas.create_oval(x-10, y, x+10, y+20, fill=color)
+        canvas.create_text(x, y+15, text=self.getNom(), fill="black")
+        canvas.config(scrollregion=(-taille[0], 0, taille[1], 0))
+        if self.getGauche() != None and self.getDroit() != None:
+            self.getGauche().dessinerArbre(canvas, largeur_canvas, x-posXSuivant, posYSuivant, cpt+1, largeur, profondeur, taille, posXSuivant/2)
+            self.getDroit().dessinerArbre(canvas, largeur_canvas, x+posXSuivant, posYSuivant, cpt+1, largeur, profondeur, taille, posXSuivant/2)
 
-
-test = comptage_lettre("bonjour")
+#test = comptage_lettre("salutation")
 #print(test)
 #test = {"o" : 2, "b" : 1, "j" : 1, "u" : 1}
-l = Sommet.make_list(test)
+#l = Sommet.make_list(test)
 #print(l)
 
 
-a = ArbreB.create_tree(l)
+#a = ArbreB.create_tree(l)
 #cd = ArbreB.codage_arbre(a)
 #print(a.racine.affichage())
 #print(a.racine.fils_gauche.affichage(), a.racine.fils_droit.affichage())
@@ -126,3 +182,6 @@ a = ArbreB.create_tree(l)
 #print(test)
 #a = a.suppression_sommet("o", l)
 #print(a.codage())
+#print(a.profondeur_arbre())
+#print(a.largeur_arbre())
+#print(a.largeur_arbre())
